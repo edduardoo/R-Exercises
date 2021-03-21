816e518e4354a5a0a82dfb91bb3734d033b06ece
Eduardo Jardim <duhjardim@hotmail.com>
2021-03-21 13:50:34 -0300

    finish exercise 1

--- Answers/Exercise-1.R
+++ Answers/Exercise-1.R
@@ -1,1 +1,16 @@
+# Exercise 1 - Using function runif() generate 20 random numbers between:
+# 0 and 1
 runif(20)
+
+#-7 and 5
+runif(20, -7, 5)
+
+# 50 and 200
+runif(20, 50, 200)
+
+
+# P.S.: I could also name the arguments e.g. runif(n=20, min=50, max=200)
+
+
+
+
